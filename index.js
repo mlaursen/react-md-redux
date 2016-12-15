754f7901f12bdf701d092329caaad233c51995f9
Mikkel Laursen <mikkel.laursen@fourwindsinteractive.com>
2016-12-15 10:50:54 -0700

    Updated the defaults for SSR

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('babel-core/register'); // Enable runtime transpilation to use ES6/7 in 
 
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
-global.__DISABLE_SSR__ = true;
+global.__DISABLE_SSR__ = false;
 global.__DEV__ = process.env.NODE_ENV !== 'production';
 
 const WebpackIsomorphicTools = require('webpack-isomorphic-tools');
